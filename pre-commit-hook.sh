13da7a23deb72aa16cdf1810acb00e21288c6011
Alejandro Jimenez <ajimenez@decentraland.org>
2021-02-25 12:30:26 +0100

    Now --include option works properly

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -16,10 +16,14 @@ echo "Staged files ${STAGED_FILES}"
 # Build edit string, by replacing newlines with semicolons.
 # --diff-filter=d only filters files that are not deleted, which means we won't have trouble adding them afterwards
 INCLUDE_STRING=`git diff --name-only --cached --diff-filter=d | sed ':a;N;$!ba;s/\n/;/g'`
-echo "Include string: $INCLUDE_STRING"
+
+# The --include option requires paths relative to the solution, we replace the path to it  
+INCLUDE_STRING_FILTERED=${INCLUDE_STRING//unity-client\//}
+
+echo "Include string: $INCLUDE_STRING_FILTERED"
 
 # If the include string is empty, we're done. This happens e.g. if the commit only consists of deleted files.
-if [[ -z "$INCLUDE_STRING" ]]
+if [[ -z "$INCLUDE_STRING_FILTERED" ]]
 then
     echo "No files to change"
     exit 0
@@ -27,14 +31,15 @@ fi
 
 # Edit your project files here
 echo "Formatting files..."
+SOLUTION_FILE=$(find . -type f -name "unity-client.sln")
 if [[ "$OSTYPE" == "msys"* ]]; then
     # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-    ./.git/hooks/resharper/cleanupcode.exe --settings="./unity-client/unity-client.sln.DotSettings" --profile="Reformat Code DCL" ./unity-client/unity-client.sln --include="$INCLUDE_STRING"
+    ./.git/hooks/resharper/cleanupcode.exe --settings="./unity-client/unity-client.sln.DotSettings" --profile="Reformat Code DCL" ./unity-client/unity-client.sln --include="$INCLUDE_STRING_FILTERED"
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     #Cygwin terminal emulator
-    ./.git/hooks/resharper/cleanupcode.exe --settings="./unity-client/unity-client.sln.DotSettings" --profile="Reformat Code DCL" ./unity-client/unity-client.sln --include="$INCLUDE_STRING"
+    ./.git/hooks/resharper/cleanupcode.exe --settings="./unity-client/unity-client.sln.DotSettings" --profile="Reformat Code DCL" ./unity-client/unity-client.sln --include="$INCLUDE_STRING_FILTERED"
 else
-    sh ./.git/hooks/resharper/cleanupcode.sh --settings="./unity-client/unity-client.sln.DotSettings" --profile="Reformat Code DCL" ./unity-client/unity-client.sln --include="$INCLUDE_STRING"
+    sh ./.git/hooks/resharper/cleanupcode.sh --settings="./unity-client/unity-client.sln.DotSettings" --profile="Reformat Code DCL" ./unity-client/unity-client.sln --include="$INCLUDE_STRING_FILTERED"
 fi
 
 
